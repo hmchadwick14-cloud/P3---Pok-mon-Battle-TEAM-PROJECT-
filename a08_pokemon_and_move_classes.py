f52ef331e72fe16ea8018625410c98285e33c010
monicaarias427 <monica.celeste.arias@gmail.com>
2026-03-27 13:43:02 -0600

    Update a08_pokemon_and_move_classes.py

--- a08_pokemon_and_move_classes.py
+++ a08_pokemon_and_move_classes.py
@@ -4,5 +4,17 @@
 
 import random
 
+class Pokemon:
+    def __init__(self, name, elemental_type, hit_points):
+        self.name = name
+        self.elemental_type = elemental_type
+        self.hit_points = 0
 
+    def get_info(self):
+        return f"{self.name.title()} - Type: {self.elemental_type} - Hit Points: {self.hit_points}"
+        
+    
+    def heal(self):
+        self.hit_points+=15
+        print(f"{self.name.title()} has been healed to {self.hit_points} points. ")
 
